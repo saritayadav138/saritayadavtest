ebc66d632332ba7cd97a3df49d562db5636ab117
saritayadav138 <shalu13.mpec@gmial.com>
2024-09-06 13:17:05 +0530

    new add

--- node.js
+++ node.js
@@ -1,22 +1,73 @@
-function arrobj(arr)
+// function arrobj(arr)
+// {
+//     let obj = {};
+//     arr.forEach((v) => {
+//         let  key=v[0];
+//         let value=v[1];
+//         obj[key]=value;});
+//         return obj;
+
+//     }
+
+    
+// console.log(
+//     arrobj([
+//         ["ram", 1],
+//         ["sita",2],
+//         ["gita",3],
+//     ])
+// );
+
+    
+function bulb()
 {
-    let obj = {};
-    arr.forEach((v) => {
-        let  key=v[0];
-        let value=v[1];
-        obj[key]=value;});
-        return obj;
+    let c=document.getElementById("text1").ariaValueMax;
+    if(c==1)
+    {
+        var img1=document.createElement("img1");
+        img1.src="4-dots.png";
+        var src=document.getElementsByClassName("div1");
+        src.appendChild(img1);
 
     }
+else{
+    if(c==2)
+    {var img1=document.createElement("img1");
+        img1.src="4-dots.png";
+        var src=document.getElementsByClassName("div1");
+        src.appendChild(img1);
 
-    
-console.log(
-    arrobj([
-        ["ram", 1],
-        ["sita",2],
-        ["gita",3],
-    ])
-);
 
+        var img1=document.createElement("img1");
+        img1.src="4-dots.png";
+        var src=document.getElementsByClassName("div1");
+        src.appendChild(img1);
+
+    }
+    else{
+        if(b==3)
+        {
+            var img1=document.createElement("img1");
+        img1.src="4-dots.png";
+        var src=document.getElementsByClassName("div1");
+        src.appendChild(img1);
+
+        var img1=document.createElement("img1");
+        img1.src="4-dots.png";
+        var src=document.getElementsByClassName("div1");
+        src.appendChild(img1);
+
+        var img1=document.createElement("img1");
+        img1.src="4-dots.png";
+        var src=document.getElementsByClassName("div1");
+        src.appendChild(img1);
+
+
+        }
+            
+        
+    }
     
+}
+}
 
